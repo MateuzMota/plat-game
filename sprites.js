46bb4c5a0e27508f525ab8295602801d5c98d107
jamelsalah <57234834+jamelsalah@users.noreply.github.com>
2021-05-06 23:31:34 -0300

    canvas border

--- sprites.js
+++ sprites.js
@@ -1,13 +1,1 @@
-function sprite(img, x, y, w, h) {
-    function draw(ctx, xcanvas, ycanvas, w = this.w, h = this.h) {
-        ctx.drawImage(img, this.x, this.y, this.w, this.h, xcanvas, ycanvas, w, h);
-    }
 
-    return {
-        x,
-        y,
-        w,
-        h,
-        draw
-    }
-}
