2938207de71f8b13260539e66abe37ac3fd22835
Mateus Mota <m4t3u5.m0t4@gmail.com>
2021-05-07 01:53:22 -0300

    add static file to client

---
+++ client/src/app.js
@@ -0,0 +1,18 @@
+// function() {
+
+//     IO {
+//         All code related to Socket.IO connections goes here.
+//     }
+
+//     App {
+//         Generic game logic code.
+
+//         Host {
+//             Game logic for the 'Host' (big) screen.
+//         }
+
+//         Player {
+//             Game logic specific to 'Player' screens.
+//         }
+//     }       
+// }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app).listen(8080);
-const socket = require('socket.io').listen(server);
+const server = require('http').createServer(app);
+const socketio = require('socket.io')
+
+const sockets = socketio(server);
+
+app.use(express.static("../client/src"));
 
 const state = {
     players: []
 }
 
-socket.on('connection', (socket) => {
-    const playerId = socket.id
+sockets.on('connection', (socket) => {
+    const playerId = socket.id;
     console.log(`Player ID: ${playerId}`);
+})
+
+server.listen(3000, () => {
+    console.log('running...')
 })
